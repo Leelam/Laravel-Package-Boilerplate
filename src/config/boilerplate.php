0c5ade30c15f275d1fd8e4c245f5a0c644e0e590
GKrishna M <gm@leelam.com>
2015-11-29 02:57:25 +0530

    Updated Config file

--- src/config/boilerplate.php
+++ src/config/boilerplate.php
@@ -6,3 +6,83 @@
  * Date: 28/11/15
  * Time: 12:51 PM
  */
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable Boilerplate
+    |--------------------------------------------------------------------------
+    |
+    | This is part will the people about functionality of the given by package
+    | Settings and it can be override by local copy.
+    |
+    |
+    */
+
+    'enable' => env('BOILERPLATE_ENABLE', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Route
+    |--------------------------------------------------------------------------
+    |
+    | Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    | IT has been the industry's standard dummy text ever since the 1500s.
+    | A galley of type and scrambled it to make a type specimen book.
+    |
+    */
+
+    'route' => 'boilerplates',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Singular
+    |--------------------------------------------------------------------------
+    |
+    | Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    | IT has been the industry's standard dummy text ever since the 1500s.
+    | A galley of type and scrambled it to make a type specimen book.
+    |
+    */
+
+    'singular' => 'boilerplate',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Title or Model
+    |--------------------------------------------------------------------------
+    |
+    | Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    | IT has been the industry's standard dummy text ever since the 1500s.
+    | A galley of type and scrambled it to make a type specimen book.
+    |
+    */
+
+    'title' => 'Boilerplate',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Table
+    |--------------------------------------------------------------------------
+    |
+    | Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    | IT has been the industry's standard dummy text ever since the 1500s.
+    | A galley of type and scrambled it to make a type specimen book.
+    |
+    */
+
+    'table' => 'boilerplates',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Anything that can right
+    |--------------------------------------------------------------------------
+    |
+    | Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+    | IT has been the industry's standard dummy text ever since the 1500s.
+    | A galley of type and scrambled it to make a type specimen book.
+    |
+    */
+
+    'anything' => 'anyThing',
+];
